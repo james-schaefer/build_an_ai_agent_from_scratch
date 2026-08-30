fc693ced5eb393e94991fd680d66b58a9e86974b
james schaefer <jrs@slow.camp>
2026-08-30 15:54:52 -0700

    code listings: 2.1, 2.2, 2.5, 2.6

---
+++ 02/listing_2.1.py
@@ -0,0 +1,11 @@
+# Build an AI From Scratch
+# Listing 2.1, pg 23
+#
+# from the root of the project: 
+# ```
+#   uv sync
+#   uv run python 02/listing_2.1.py
+# ```
+
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
